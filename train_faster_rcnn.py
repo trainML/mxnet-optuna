6d8180ee12dbb7ef903c2532646a2fb9bc431353
Adam Kowalski <adam@trainml.ai>
2021-01-12 13:59:53 -0600

    trying unpooled gpu memory type

--- train_faster_rcnn.py
+++ train_faster_rcnn.py
@@ -10,7 +10,7 @@ import os
 
 # disable autotune
 os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
-os.environ['MXNET_GPU_MEM_POOL_TYPE'] = 'Round'
+os.environ['MXNET_GPU_MEM_POOL_TYPE'] = 'Unpooled'
 os.environ['MXNET_GPU_MEM_POOL_ROUND_LINEAR_CUTOFF'] = '26'
 os.environ['MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_FWD'] = '999'
 os.environ['MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_BWD'] = '25'
